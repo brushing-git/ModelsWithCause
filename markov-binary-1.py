57bdfb529f328ff3e32670c03c354666bcaf8199
crobers0 <110861583+crobers0@users.noreply.github.com>
2024-02-21 14:55:40 -0500

    Update markov-binary-1.py

--- markov-binary-1.py
+++ markov-binary-1.py
@@ -33,7 +33,12 @@ training_d = training_d.astype(np.single)
 
 # print parameters
 f = open('markov-binary-1-parameters.txt', 'w')
-f.write('')
+f.write("number of coins: %d\n", len(coins))
+f.write("biases of coins (respectively): ", biases, "\n")
+f.write("transition probabilities for each coin (respectively): ", transition, "\n")
+f.write("procedure:\n\t1. set initial coin state to one of %d coins.\n", len(coins))
+f.write("\t2. obtain set of probabilities based on previous coin state. each coin has a unique set of probabilities to transition to another coin. in this sample, the coin state cannot repeat itself.\n")
+f.write("\t3. transition to another coin based on this set.\n\t4. perform 10 trials (flips) of the new coin based on its unique bias.\n\t5. repeat.")
 f.close()
 
 np.savetxt("markov-binary-1-training.txt", training_d, delimiter="", newline=",", fmt='%d')
